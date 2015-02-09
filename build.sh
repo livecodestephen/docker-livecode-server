491e44383035b0c79de5588a4c53785e2b0c28fd
Stephen Tawn <stephen.tawn@livecode.com>
2015-02-09 17:26:28 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+###############################################################################
+export MAINTAINER="Stephen Tawn <stephen.tawn@livecode.com>"
+export NAMESPACE=livecodestephen/
+export DATE=$(date +"%F")
+
+export REPO=https://github.com/livecodestephen/livecode.git
+export BRANCH=feature-install-server
+export COMMIT=$(git ls-remote $REPO $BRANCH | awk '{ print $1 }') || exit 1
+
+export TAG=$BRANCH-$DATE
+
+export BASE=fedora:21
+
+###############################################################################
+
+function die() {
+    echo "$*" 1>&2
+    exit 1
+}
+
+function make_dockerfile() {
+    mkdir -p $1/$TAG
+    cat $1/Dockerfile.template \
+	| envsubst '$MAINTAINER $NAMESPACE $DATE $REPO $BRANCH $COMMIT $TAG $BASE'\
+	> $1/$TAG/Dockerfile
+}
+
+###############################################################################
+
+# Set the working dir to the location of this script
+cd $(dirname $(readlink -f "$0"))
+
+# Check that there have been changes since the previous build
+if [ -a previous-commit ] && [ "$(cat previous-commit)" = "$COMMIT" ]; then
+    echo "No commited changes since previous build."
+    exit 0
+fi
+echo $COMMIT > previous-commit
+
+
+########################################
+# Make the Dockerfiles
+
+make_dockerfile livecode-server-build
+make_dockerfile livecode-server
+
+
+########################################
+# Make the containers
+
+echo "Building ${NAMESPACE}livecode-server-build:$TAG"
+docker build -t ${NAMESPACE}livecode-server-build:$TAG livecode-server-build/$TAG \
+    || die "Build failed!"
+
+echo "Extracting livecode server"
+docker run -i --rm l${NAMESPACE}ivecode-server-build:$TAG /usr/bin/tar -czP /opt/livecode \
+    > livecode-server/$TAG/livecode-server.tar.gz \
+    || die "Extracting /opt/livecode failed!"
+
+echo "Building ${NAMESPACE}livecode-server:$TAG"
+docker build -t ${NAMESPACE}livecode-server:$TAG livecode-server/$TAG \
+    || die "Build failed!"
+
+docker tag ${NAMESPACE}livecode-server-build:$TAG ${NAMESPACE}livecode-server-build:latest
+docker tag ${NAMESPACE}livecode-server:$TAG ${NAMESPACE}livecode-server:latest
+
+
+########################################
+# Push the images
+
+docker push ${NAMESPACE}livecode-server-build \
+    || die "Failed to push ${NAMESPACE}livecode-server-build!"
+docker push ${NAMESPACE}livecode-server \
+    || die "Failed to push ${NAMESPACE}livecode-server-build!"
+
+
+########################################
+# Update git
+
+git add previous-commit
+git add livecode-server-build/$TAG/Dockerfile
+git add livecode-server/$TAG/Dockerfile
+git commit -m "Built $TAG"
+git push
+
